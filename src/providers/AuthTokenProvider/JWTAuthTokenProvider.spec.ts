96638b2ff6475f706a4e930f4c323f4e387b15df
carloshkruger <carloshkruger@gmail.com>
2022-02-28 16:11:08 -0300

    chore: created test for decrypt method

--- src/providers/AuthTokenProvider/JWTAuthTokenProvider.spec.ts
+++ src/providers/AuthTokenProvider/JWTAuthTokenProvider.spec.ts
@@ -1,4 +1,4 @@
-import { sign } from 'jsonwebtoken'
+import * as jwt from 'jsonwebtoken'
 import { JWTAuthTokenProvider } from './JWTAuthTokenProvider'
 
 jest.mock('jsonwebtoken')
@@ -8,13 +8,23 @@ describe('JWTAuthTokenProvider', () => {
     it('should call sign method with correct values', () => {
       new JWTAuthTokenProvider().generate('1234')
 
-      expect(sign).toHaveBeenCalledWith(
+      expect(jwt.sign).toHaveBeenCalledWith(
         { userId: '1234' },
         process.env.JWT_SECRET,
         {
           expiresIn: '1h'
         }
       )
+    })
+  })
+  describe('decrypt', () => {
+    it('should call verify method with correct values', () => {
+      jest.spyOn(jwt, 'verify').mockReturnValue({
+        userId: '1234'
+      } as any)
+      new JWTAuthTokenProvider().decrypt('token')
+
+      expect(jwt.verify).toHaveBeenCalledWith('token', process.env.JWT_SECRET)
     })
   })
 })
