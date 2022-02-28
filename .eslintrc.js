ab9d8c8cef475ff9b47935b852afc813d41b70df
carloshkruger <carloshkruger@gmail.com>
2022-02-28 15:52:42 -0300

    chore: disabled no-explicit-any on eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,27 @@
 module.exports = {
   env: {
-  es2021: true,
+    es2021: true,
     node: true,
-    jest: true,
+    jest: true
   },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier"
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: [, '@typescript-eslint', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
+    '@typescript-eslint/no-explicit-any': 'off'
   },
   settings: {
     'import/resolver': {
-      typescript: {},
-    },  
-  },
-};
+      typescript: {}
+    }
+  }
+}
