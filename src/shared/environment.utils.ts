702ba92bdc14cf6001a996518717bcef8834c19c
carloshkruger <carloshkruger@gmail.com>
2022-03-08 21:42:47 -0300

    chore: added NODE_ENV variable and environment utils

---
+++ src/shared/environment.utils.ts
@@ -0,0 +1,4 @@
+export const isDevEnvironment = () => process.env.NODE_ENV === 'env'
+export const isProductionEnvironment = () =>
+  process.env.NODE_ENV === 'production'
+export const isTestEnvironment = () => process.env.NODE_ENV === 'test'
